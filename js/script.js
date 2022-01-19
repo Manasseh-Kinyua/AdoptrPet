0643628c5a0f6b56f3cf556f70a77ff2eec2871d
Njoro410 <brian.njoroge@student.moringaschool.com>
2022-01-19 12:52:45 +0300

    added classes to data being appended in jQuery

--- js/script.js
+++ js/script.js
@@ -9,11 +9,11 @@ $(document).ready(function () {
         $("#animalDetails").modal('show');
         $("#pic").append('<img src="./assets/images/yuki.jpg" width="100%" alt="">');
         $("#name").append('YUKI');
-        $("#breed").append('Kenyan Shepherd');
-        $("#gender").append('Female');
-        $("#age").append('2 Years');
-        $("#vaccination").append('Fully Vaccinated');
-        $("#description").append('<br/>' + '2 year old female, playful; fully vaccinated, spayed and is ready for a new home.')
+        $("#breed").append('<p class=title>BREED:</p>'+'<hr class=line>'+'<p class=details> Kenyan Shepherd </p>');
+        $("#gender").append('<p class=title>GENDER:</p>'+'<p class=details> Female </p>');
+        $("#age").append('<p class=title>AGE:</p>'+'<p class=details> 24 Months </p>');
+        $("#vaccination").append('<p class=title>VACCINATION:</p>'+'<p class=details> Fully Vaccinated </p>');
+        $("#description").append('<P class=title>DESCRIPTION:</p>'+'<hr class=line>'+'<p class=details>2 year old female, playful; fully vaccinated, spayed and is ready for a new home.')
 
     })
 
@@ -21,11 +21,11 @@ $(document).ready(function () {
         $("#animalDetails").modal('show');
         $("#pic").append('<img src="./assets/images/jojo.jpg" width="100%" alt="">');
         $("#name").append('JOJO');
-        $("#breed").append('Perusian Cat');
-        $("#gender").append('Female');
-        $("#age").append('8 months old');
-        $("#vaccination").append('Pending Vaccination');
-        $("#description").append('<br/>' + 'A very shy gorgeous cat that loves to play and nap all day, she will also get rid of your unwanted guests')
+        $("#breed").append('<p class=title>BREED:</p>'+'<hr class=line>'+'<p class=details> Persian Cat</p>');
+        $("#gender").append('<p class=title>GENDER:</p>'+'<p class=details> Female </p>');
+        $("#age").append('<p class=title>AGE:</p>'+'<p class=details> 24 Months </p>');
+        $("#vaccination").append('<p class=title>VACCINATION:</p>'+'<p class=details> Pending Vaccination </p>');
+        $("#description").append('<P class=title>DESCRIPTION:</p>'+'<hr class=line>'+'<p class=details>A very shy gorgeous cat that loves to play and nap all day, she will also get rid of your unwanted guests')
 
     })
 
@@ -33,11 +33,11 @@ $(document).ready(function () {
         $("#animalDetails").modal('show');
         $("#pic").append('<img src="./assets/images/apollo.jpg" width="100%" alt="">');
         $("#name").append('APOLLO');
-        $("#breed").append('Street Dog');
-        $("#gender").append('Male');
-        $("#age").append('3 months old');
-        $("#vaccination").append('Pending Vaccination');
-        $("#description").append('<br/>' + 'Playful. You will need to bring him back for his final round of shots in two weeks')
+        $("#breed").append('<p class=title>BREED:</p>'+'<hr class=line>'+'<p class=details> Street Dog</p>');
+        $("#gender").append('<p class=title>GENDER:</p>'+'<p class=details> Male </p>');
+        $("#age").append('<p class=title>AGE:</p>'+'<p class=details> 3 Months </p>');
+        $("#vaccination").append('<p class=title>VACCINATION:</p>'+'<p class=details> Pending Vaccination </p>');
+        $("#description").append('<P class=title>DESCRIPTION:</p>'+'<hr class=line>'+'<p class=details>Playful. You will need to bring him back for his final round of shots in two weeks')
 
     })
 
@@ -45,11 +45,11 @@ $(document).ready(function () {
         $("#animalDetails").modal('show');
         $("#pic").append('<img src="./assets/images/kiwi.jpg" width="100%" alt="">');
         $("#name").append('KIWI');
-        $("#breed").append('Domestic Short Haired Cat');
-        $("#gender").append('Male');
-        $("#age").append('11 months old');
-        $("#vaccination").append('Vaccinated');
-        $("#description").append('<br/>' + 'A very shy gorgeous cat that loves to play and nap all day, she will also get rid of your unwanted guests')
+        $("#breed").append('<p class=title>BREED:</p>'+'<hr class=line>'+'<p class=details> Domestic Short Haired Cat</p>');
+        $("#gender").append('<p class=title>GENDER:</p>'+'<p class=details> Male </p>');
+        $("#age").append('<p class=title>AGE:</p>'+'<p class=details> 11 Months </p>');
+        $("#vaccination").append('<p class=title>VACCINATION:</p>'+'<p class=details> Vaccinated </p>');
+        $("#description").append('<P class=title>DESCRIPTION:</p>'+'<hr class=line>'+'<p class=details>A very shy gorgeous cat that loves to play and nap all day, she will also get rid of your unwanted guests')
 
     })
 
@@ -57,11 +57,11 @@ $(document).ready(function () {
         $("#animalDetails").modal('show');
         $("#pic").append('<img src="./assets/images/nala.jpg" width="100%" alt="">');
         $("#name").append('NALA');
-        $("#breed").append('Domestic Short Haired Cat');
-        $("#gender").append('Female');
-        $("#age").append('16 months old');
-        $("#vaccination").append('Pending Vaccination');
-        $("#description").append('<br/>' + 'A very shy gorgeous cat that loves to play and nap all day, she will also get rid of your unwanted guests')
+        $("#breed").append('<p class=title>BREED:</p>'+'<hr class=line>'+'<p class=details> Domestic Short Haired Cat</p>');
+        $("#gender").append('<p class=title>GENDER:</p>'+'<p class=details> Female </p>');
+        $("#age").append('<p class=title>AGE:</p>'+'<p class=details> 16 Months </p>');
+        $("#vaccination").append('<p class=title>VACCINATION:</p>'+'<p class=details> Pending Vaccination </p>');
+        $("#description").append('<P class=title>DESCRIPTION:</p>'+'<hr class=line>'+'<p class=details>A very shy gorgeous cat that loves to play and nap all day, she will also get rid of your unwanted guests')
 
     })
 
@@ -69,11 +69,11 @@ $(document).ready(function () {
         $("#animalDetails").modal('show');
         $("#pic").append('<img src="./assets/images/atlas.jpg" width="100%" alt="">');
         $("#name").append('ATLAS');
-        $("#breed").append('Vulnerable Native Breeds');
-        $("#gender").append('Female');
-        $("#age").append('18 months old');
-        $("#vaccination").append('Vaccinated');
-        $("#description").append('<br/>' + 'A very shy gorgeous cat that loves to play and nap all day, she will also get rid of your unwanted guests')
+        $("#breed").append('<p class=title>BREED:</p>'+'<hr class=line>'+'<p class=details> Vulnerable Native Breed</p>');
+        $("#gender").append('<p class=title>GENDER:</p>'+'<p class=details> Male </p>');
+        $("#age").append('<p class=title>AGE:</p>'+'<p class=details> 18 Months </p>');
+        $("#vaccination").append('<p class=title>VACCINATION:</p>'+'<p class=details> Vaccinated </p>');
+        $("#description").append('<P class=title>DESCRIPTION:</p>'+'<hr class=line>'+'<p class=details>A very shy gorgeous cat that loves to play and nap all day, she will also get rid of your unwanted guests')
 
     })
 
